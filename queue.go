c38e24dd6532e2be9396fce8b27dccd3f5b7941e
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2015-10-29 14:05:12 -0200

    Interfaces lock

--- queue.go
+++ queue.go
@@ -2,87 +2,39 @@ package gommons
 
 import "sync"
 
-type node struct {
-	data interface{}
-	next *node
+type Event struct {
+	data int
 }
 
-//  A go-routine safe FIFO (first in first out) data stucture.
 type Queue struct {
-	head  *node
-	tail  *node
-	count int
-	sync.RWMutex
+	queue []*Event
+	sync.Mutex
 }
 
-//  Creates a new pointer to a new queue.
-func NewQueue() *Queue {
-	return &Queue{}
-}
+func (q Queue) Len() int           { return len(q.queue) }
+func (q Queue) Less(i, j int) bool { return true }
+func (q Queue) Swap(i, j int)      {}
 
-//  Returns the number of elements in the queue (i.e. size/length)
-//  go-routine safe.
-func (q *Queue) Len() int {
-	q.RLock()
-	defer q.RUnlock()
-	return q.count
-}
-
-//  Pushes/inserts a value at the end/tail of the queue.
-//  Note: this function does mutate the queue.
-//  go-routine safe.
-func (q *Queue) Push(item interface{}) {
+func (q *Queue) Push(x interface{}) {
 	q.Lock()
 	defer q.Unlock()
 
-	n := &node{data: item}
-
-	if q.tail == nil {
-		q.tail = n
-		q.head = n
-	} else {
-		q.tail.next = n
-		q.tail = n
-	}
-
-	q.count++
+	item := x.(*Event)
+	q.queue = append(q.queue, item)
 }
 
-//  Returns the value at the front of the queue.
-//  i.e. the oldest value in the queue.
-//  Note: this function does mutate the queue.
-//  go-routine safe.
 func (q *Queue) Pop() interface{} {
 	q.Lock()
 	defer q.Unlock()
 
-	if q.head == nil {
+	old := q
+	n := len(old.queue)
+
+	if len(old.queue) < 1 {
 		return nil
 	}
 
-	n := q.head
-	q.head = n.next
-
-	if q.head == nil {
-		q.tail = nil
-	}
-
-	q.count--
-	return n.data
-}
-
-//  Returns a read value at the front of the queue.
-//  i.e. the oldest value in the queue.
-//  Note: this function does NOT mutate the queue.
-//  go-routine safe.
-func (q *Queue) Peek() interface{} {
-	q.Lock()
-	defer q.Unlock()
-
-	n := q.head
-	if n == nil || n.data == nil {
-		return nil
-	}
-
-	return n.data
+	item := old.queue[n-1]
+	q.queue = old.queue[0 : n-1]
+	return item
 }
--- queue_test.go
+++ queue_test.go
@@ -1,6 +1,7 @@
 package gommons
 
 import (
+	"container/heap"
 	"runtime"
 	"testing"
 	"time"
@@ -13,42 +14,25 @@ import (
 func TestQueue(t *testing.T) {
 	g := Goblin(t)
 	g.Describe("Basic Queue test", func() {
-		g.It("Should create queue ", func() {
-			q := NewQueue()
-			g.Assert(q != nil).IsTrue()
-		})
+		g.It("Should push and pop data ", func() {
+			queue := &Queue{}
+			heap.Init(queue)
+			heap.Push(queue, &Event{data: 1})
+			heap.Push(queue, &Event{data: 2})
+			heap.Push(queue, &Event{data: 3})
 
-		g.It("Should push data ", func() {
-			q := NewQueue()
-			q.Push(10)
-		})
-
-		g.It("Should pop data ", func() {
-			q := NewQueue()
-			q.Push(10)
-			time.Sleep(time.Millisecond * 100)
-			data := q.Pop()
+			data := heap.Pop(queue).(*Event)
 			g.Assert(data != nil).IsTrue()
+
+			data = heap.Pop(queue).(*Event)
+			g.Assert(data != nil).IsTrue()
+
+			data = heap.Pop(queue).(*Event)
+			g.Assert(data != nil).IsTrue()
+
+			d := heap.Pop(queue)
+			g.Assert(d == nil).IsTrue()
 		})
-
-		g.It("Should pop many data ", func() {
-			q := NewQueue()
-			q.Push(10)
-			q.Push(11)
-			q.Push(12)
-
-			time.Sleep(time.Millisecond * 100)
-
-			data := q.Pop()
-			g.Assert(data == 10).IsTrue()
-
-			data = q.Pop()
-			g.Assert(data == 11).IsTrue()
-
-			data = q.Pop()
-			g.Assert(data == 12).IsTrue()
-		})
-
 	})
 }
 
@@ -57,22 +41,22 @@ func TestQueueConcurrency(t *testing.T) {
 	g.Describe("Concurrency test", func() {
 		g.It("Should test concurrency with 1 CPU", func() {
 			runtime.GOMAXPROCS(1)
-			q := NewQueue()
-			g.Assert(q != nil).IsTrue()
+			queue := &Queue{}
+			heap.Init(queue)
 
 			buffer := []int{}
 
 			for i := 0; i < 1000; i++ {
-				go func() {
-					q.Push(i)
-				}()
+				go func(v int) {
+					heap.Push(queue, &Event{data: v})
+				}(i)
 			}
 
 			for {
 				go func() {
-					data := q.Pop()
+					data := heap.Pop(queue)
 					if data != nil {
-						buffer = append(buffer, data.(int))
+						buffer = append(buffer, data.(*Event).data)
 					}
 				}()
 
@@ -85,29 +69,29 @@ func TestQueueConcurrency(t *testing.T) {
 			g.Assert(len(buffer) >= 1000).IsTrue()
 		})
 
-		g.It("Should test concurrency with 2 >= CPUs", func() {
+		g.It("Should test concurrency with more than 1 CPU", func() {
 			cpus := runtime.NumCPU()
 			if cpus == 1 {
 				cpus = 2
 			}
 
 			runtime.GOMAXPROCS(cpus)
-			q := NewQueue()
-			g.Assert(q != nil).IsTrue()
+			queue := &Queue{}
+			heap.Init(queue)
 
 			buffer := []int{}
 
 			for i := 0; i < 1000; i++ {
-				go func() {
-					q.Push(i)
-				}()
+				go func(v int) {
+					heap.Push(queue, &Event{data: v})
+				}(i)
 			}
 
 			for {
 				go func() {
-					data := q.Pop()
+					data := heap.Pop(queue)
 					if data != nil {
-						buffer = append(buffer, data.(int))
+						buffer = append(buffer, data.(*Event).data)
 					}
 				}()
 
